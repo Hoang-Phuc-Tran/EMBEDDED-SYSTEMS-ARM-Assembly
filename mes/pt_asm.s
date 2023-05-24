d061175b407d15740119a4e66a850ccf56594d89
Hoang Phuc Tran <hoangphuctran9102@gmail.com>
2023-05-23 22:04:15 -0400

    Add a function declaration and Create a new function in C hook file called _pt_A2

--- mes/pt_asm.s
+++ mes/pt_asm.s
@@ -16,34 +16,6 @@
 
 @ instructions. The default is divided (separate instruction sets)
     .global add_test @ Make the symbol name for the function visible to the linker
-    .global busy_delay
-
-    .code 16 @ 16bit THUMB code (BOTH .code and .thumb_func are required)
-    .thumb_func @ Specifies that the following symbol is the name of a THUMB
-
-@ encoded function. Necessary for interlinking between ARM and THUMB code.
-    .type add_test, %function @ Declares that the symbol is a function (not strictly required)
-    @ Actual declaration of the symbol
-    @ Data section - initialized values
-
-@ Test code for my own new function called from C
-@ This is a comment. Anything after an @ symbol is ignored.
-@@ This is also a comment. Some people use double @@ symbols.
-    .code 16 @ This directive selects the instruction set being generated.
-
-@ The value 16 selects Thumb, with the value 32 selecting ARM.
-    .text @ Tell the assembler that the upcoming section is to be considered
-
-@ assembly language instructions - Code section (text -> ROM)
-@@ Function Header Block
-    .align 2 @ Code alignment - 2^n alignment (n=2)
-
-@ This causes the assembler to use 4 byte alignment
-    .syntax unified @ Sets the instruction set to the new unified ARM + THUMB
-
-@ instructions. The default is divided (separate instruction sets)
-    .global add_test @ Make the symbol name for the function visible to the linker
-    .global busy_delay
 
     .code 16 @ 16bit THUMB code (BOTH .code and .thumb_func are required)
     .thumb_func @ Specifies that the following symbol is the name of a THUMB
@@ -106,6 +78,50 @@
     pop {r5}
     bx lr @ Return (Branch eXchange) to the address in the link register (lr)
 
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @ Assembly file ended by single .end directive on its own line
+    .code 16 @ This directive selects the instruction set being generated.
+
+@ The value 16 selects Thumb, with the value 32 selecting ARM.
+    .text @ Tell the assembler that the upcoming section is to be considered
+
+@ assembly language instructions - Code section (text -> ROM)
+@@ Function Header Block
+    .align 2 @ Code alignment - 2^n alignment (n=2)
+
+@ This causes the assembler to use 4 byte alignment
+    .syntax unified @ Sets the instruction set to the new unified ARM + THUMB
+
+@ instructions. The default is divided (separate instruction sets)
+    .global pt_led_demo_a2
+
+    .code 16 @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .thumb_func @ Specifies that the following symbol is the name of a THUMB
+
+@ encoded function. Necessary for interlinking between ARM and THUMB code.
+    .type pt_led_demo_a2, %function @ Declares that the symbol is a function (not strictly required)
+    @ Actual declaration of the symbol
+    @ Data section - initialized values
+
+@ Function Declaration : int add_test(int x, int y)
+@
+@ Input: r0, r1 (i.e. r0 holds x, r1 holds y)
+@ Returns: r0
+@
+@ Here is the actual add_test function
+    pt_led_demo_a2:
+    push {r4,r5, lr}    @ push the r4, r5 and lr register on the stack
+   
+    
+    pop {r4,r5, lr}      @ pop the r4,r5 and lr register out of the stack
+    bx lr                @ Return (Branch eXchange) to the address in the link register (lr)
+
+
+
+    .size add_test, .-add_test @@ - symbol size (not strictly required, but makes the debugger happy)
+
+
     .end
     Things past the end directive are not processed, as you can see here.
